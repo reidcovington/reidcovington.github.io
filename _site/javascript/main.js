dc0cfd37fb8e53001854e9e7867c8dde0ab8c7dc
Reid Covington <reid@shopcube.com>
2014-07-03 17:16:12 -0400

    Add image of 2 cats annotation

--- _site/javascript/main.js
+++ _site/javascript/main.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
-  $( document ).tooltip({
-      items: "img, [data-geo], [title]",
-      content: function() {
-        var element = $( this );
-        if ( element.is( "[data-geo]" ) ) {
-          var text = element.text();
-          return "<img class='map' alt='" + text +
-            "' src='http://maps.google.com/maps/api/staticmap?" +
-            "zoom=11&size=350x350&maptype=terrain&sensor=false&center=" +
-            text + "'>";
-        }
-        if ( element.is( "[title]" ) ) {
-          return element.attr( "title" );
-        }
-        if ( element.is( "img" ) ) {
-          return element.attr( "alt" );
-        }
-      }
+  // $( document ).tooltip({
+    $('#2cats').tooltip({
+    content: '<img src="/assets/2cats.jpg" />'
     });
 });
